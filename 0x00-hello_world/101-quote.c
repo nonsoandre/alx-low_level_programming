f3d7ca237431f332e6005ac3e541b4ab67d41ceb
nonsoandre <nonsoandre@gmail.com>
2023-09-07 18:38:48 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - Print "print without printf and puts"
+*Return: Always 0.
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
