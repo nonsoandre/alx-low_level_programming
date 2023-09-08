827111a126e3c460f162705c118ddf753cddf461
nonsoandre <nonsoandre@gmail.com>
2023-09-07 17:28:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#includes <stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
- * return always 0
- **/
+*main - entry point
+*Return: Always 0.
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
