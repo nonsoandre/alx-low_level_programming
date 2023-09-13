8faa3051f81c336971d9db87bd2a4f1658e36888
nonsoandre <nonsoandre@gmail.com>
2023-09-13 03:52:20 +0100

    5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of an integer
+ * @n: is the integer to be checked
+ * Return: retruns n
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
