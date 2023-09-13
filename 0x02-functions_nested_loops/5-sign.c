276f480089efd2258de23aae05a0be6f46b23fef
nonsoandre <nonsoandre@gmail.com>
2023-09-13 03:06:14 +0100

    4

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign - checks for the sign of a number
+ * @n: the value to check for
+ * Return: 1 if positive, Return 0 if value is 0, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		printf("+");
+		return (1);
+	if (n == 0)
+		printf("0");
+		return (0);
+	if (n < 0)
+		printf("-");
+		return (-1);
+}
