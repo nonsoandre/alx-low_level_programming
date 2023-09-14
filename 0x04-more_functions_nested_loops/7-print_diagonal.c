440847b1a74f6a6bc5290718cfb1be470dd48a40
nonsoandre <nonsoandre@gmail.com>
2023-09-14 19:48:35 +0100

    8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  prints a diagonal line on the terminal
+ * @a: value that olds the number pf times character is printed
+ */
+
+void print_diagonal(int a)
+{
+	if (a <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int b, c;
+
+		for (b = 0; b < a; b++)
+		{
+			for (c = 0; c < a; c++)
+			{
+				if (c == i)
+					_putchar('\\');
+				else if (c < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
