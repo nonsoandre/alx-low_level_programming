1b19e45cf8e209be691808bc50ccd109f1f69df6
nonsoandre <nonsoandre@gmail.com>
2023-09-14 17:47:57 +0100

    1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * main - checks for upper case
+  * @c: value to check
+  * Return: 0 or 1
+  */
+int main(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+int _isupper(int c);
+
+#endif
