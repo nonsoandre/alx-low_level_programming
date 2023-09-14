a7e9e1f92d9b342968e017bbd763f8df1a3de978
nonsoandre <nonsoandre@gmail.com>
2023-09-14 18:44:50 +0100

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_numbers - prints numbers from 0-9
+  * Return: nothing
+  */
+void print_numbers(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+		printf("%d\n", a );
+	}
+}
+
