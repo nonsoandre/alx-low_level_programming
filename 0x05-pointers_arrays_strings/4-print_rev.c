7a4762df3bdf39745bcc8708bdc4d5f7e8e3255f
nonsoandre <nonsoandre@gmail.com>
2023-09-19 02:26:08 +0100

    5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints reversed string
+ * @s: value of string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
