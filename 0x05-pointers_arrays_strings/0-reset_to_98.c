65e555f32de53049224d0593db6041421cbe37d8
nonsoandre <nonsoandre@gmail.com>
2023-09-19 01:33:29 +0100

    1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap a values for each variable
+ * @a: first value
+ * @b: second value
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	holder = *b;
+	*b = *a;
+	*a = holder;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MY_HEADERS_H
+#define MY_HEADERS_H
+
+void reset_to_98(int *n);
+void _puts(char *str);
+int _putchar(char);
+
+
+#endif
