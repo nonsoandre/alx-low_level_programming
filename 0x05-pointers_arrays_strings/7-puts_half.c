50a09f73215d18da53145fa559cf2a35d07a31e7
nonsoandre <nonsoandre@gmail.com>
2023-09-19 02:40:12 +0100

    8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - print second half of a string
+ * @str: The char array
+ * Return: nothing or void
+ */
+void puts_half(char *s)
+{
+	int strl = 0;
+	int i;
+
+	while (str[strl] != '\0')
+	{
+		strl++;
+	}
+
+	int begin;
+	if (strl % 2 == 0)
+	{
+		begin = strl / 2;
+	}
+	else
+	{
+		begin = (strl - 1) / 2;
+	}
+	for (i = begin; i < strl; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
