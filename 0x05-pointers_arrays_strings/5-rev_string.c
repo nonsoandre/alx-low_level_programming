dd267a8a19071d1d0e74510a5b7c68d74e1b2552
nonsoandre <nonsoandre@gmail.com>
2023-09-19 02:29:56 +0100

    6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - string reverse
+ * @s: value of string to be reversed
+ *
+ * Return: void
+ */
+
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char str[500];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
