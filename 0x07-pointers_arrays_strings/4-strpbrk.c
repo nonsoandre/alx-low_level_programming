b8d56f7ce810796aa132c23fb2a43644636c22f9
nonsoandre <nonsoandre@gmail.com>
2023-09-26 00:21:41 +0100

    5

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strpbrk - Search a string for any set of bytes
+* @s: first input value
+* @accept: second input value
+* Return: Always 0 (Success)
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (int j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return ('\0');
+}
