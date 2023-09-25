4933f85337ca5c8fc224c9e3c03e0bb56f9af7a1
nonsoandre <nonsoandre@gmail.com>
2023-09-26 00:23:19 +0100

    5

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,10 +8,11 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (int j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
