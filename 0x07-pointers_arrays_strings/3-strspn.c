40e77ef763af9d1869be3b019141dc9f5c14b7b4
nonsoandre <nonsoandre@gmail.com>
2023-09-26 00:10:17 +0100

    4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input value
+ * @accept: input value
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; n++)
+		{
+			if (*s == accept[a])
+			{
+				n++;
+				break;
+			}
+			else if (accept[a + 1] == '\0')
+			{
+				return (n);
+			}
+		}
+	}
+	return (n);
+}
