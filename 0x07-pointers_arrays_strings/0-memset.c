d3ed1b50a91d063e79d66993eef8698d97f2b00b
nonsoandre <nonsoandre@gmail.com>
2023-09-25 23:49:10 +0100

    1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - fill a memory block with one value
+ * @s: the memory area
+ * @b: the value
+ * @n: number of bytes to be changed
+ * Return: the new array
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
