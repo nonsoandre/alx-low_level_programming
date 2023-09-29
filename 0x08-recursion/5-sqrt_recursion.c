9f801647a2a646ca5b1085c3102204e3a861da5a
nonsoandre <nonsoandre@gmail.com>
2023-09-29 03:06:19 +0100

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,5 @@
 #include "main.h"
 /**
-* _sqrt_recursion - calculate natural square root of a number.
-* @n: value to square
-* Return: 1 if n is prime, 0 if not
-*/
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return (natural_sqrt(n, 1));
-}
-/**
 * natural_sqrt - calculate natural square root of a number.
 * @n: value to check
 * @i: iterator
@@ -29,4 +16,17 @@ int natural_sqrt(int n, int i)
 		return (-1);
 	}
 	return (natural_sqrt(n, i + 1));
+}
+/**
+* _sqrt_recursion - calculate natural square root of a number.
+* @n: value to square
+* Return: 1 if n is prime, 0 if not
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (natural_sqrt(n, 1));
 }
