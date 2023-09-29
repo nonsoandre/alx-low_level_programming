cd0eae98e634b1f743f60b380b8349369c428d38
nonsoandre <nonsoandre@gmail.com>
2023-09-29 03:20:23 +0100

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * check_palindrome - compares each character of the string.
- * @start: string
- * @end: smallest iterator.
- * Return: 1 else 0
- */
+* check_palindrome - compares each character of the string.
+* @start: string
+* @end: smallest iterator.
+* Return: 1 else 0
+*/
 int check_palindrome(char *start, char *end)
 {
 
@@ -18,7 +18,18 @@ int check_palindrome(char *start, char *end)
 	}
 	return (check_palindrome(start + 1, end - 1));
 }
-
+/**
+* find_end - detects if end of string.
+* @s: string.
+*/
+char *find_end(char *s)
+{
+	if (*s == '\0')
+	{
+		return (s - 1);
+	}
+	return (find_end(s + 1));
+}
 /**
 * is_palindrome - detects if a string is a palindrome.
 * @s: string.
