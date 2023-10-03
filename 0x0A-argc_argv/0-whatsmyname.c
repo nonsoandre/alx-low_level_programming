2cbdda2ab79665a5ad710f80e403468ca2a7a56f
nonsoandre <nonsoandre@gmail.com>
2023-10-03 03:23:46 +0100

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print name of function
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: succes compilation ok
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
