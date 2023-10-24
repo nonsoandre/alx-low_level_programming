776d3a5249b6d8bc567b4423170f231726235bb2
nonsoandre <nonsoandre@gmail.com>
2023-10-25 00:27:50 +0100

    2nd commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * listint_len - returns the number of elements
+  * @h: param h
+  * Return: number counted
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * add_nodeint - add a node at the begining
+  * @head: header, first element
+  * @n: param int to add
+  * Return: pointer of new node
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
